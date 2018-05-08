f08088420776bdca79f35a084520be15441394d2
xiaocheng <2992728450@qq.com>
2018-05-08 14:13:10 +0800

    博客思听

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,51 @@ Page({
     interval: 5000,
     duration: 1000,
     imgwidth: 320,
-    imgheight: 160
+    imgheight: 160,
+    showItem:1,
+    userInfo:'',
+  },
+  onReady:function(){
+    // if (app.globalData.userInfo) {
+    //   console.log(1)
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //   })
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回  
+    //   // 所以此处加入 callback 以防止这种情况  
+    //   console.log(2)
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //     })
+    //   }
+    // }
+    // else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理 
+    //   console.log(3)
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //       })
+    //     }
+    //   })
+    // }  
+  },
+  onLoad:function(){
+     app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+        })
+     } 
+  },
+  onShow:function(){
+    var userInfo = app.globalData.userInfo
+    this.setData({
+      userInfo: userInfo
+    })
   },
   imageLoad: function (e) {
     var windowWidth = 320;
@@ -28,6 +72,41 @@ Page({
     this.setData({
       imgwidth: windowWidth,
       imgheight: windowWidth / 2,
+    })
+  },
+  trials:function(){
+      var showItem=1;
+      this.setData({
+        showItem:showItem
+      })
+  },
+  packages: function () {
+    var showItem = 2;
+    this.setData({
+      showItem: showItem
+    })
+    
+  },
+  about: function () {
+    var showItem = 3;
+    this.setData({
+      showItem: showItem
+    })
+  },
+  vip: function () {
+    var showItem = 4;
+    this.setData({
+      showItem: showItem
+    })
+  },
+  Load:function(){
+    wx.navigateTo({
+      url: '../account/index',
+    })
+  },
+  play: function () {
+    wx.navigateTo({
+      url: '../play/index',
     })
   },
 })
--- pages/package/index.js
+++ pages/package/index.js
@@ -1,11 +1,75 @@
-//index.js
-//获取应用实例
+// pages/play/index.js
 const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    imgwidth: 700,
-    imgheight: 350
+    userInfo: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    app.userInfoReadyCallback = res => {
+      this.setData({
+        userInfo: res.userInfo,
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var userInfo = app.globalData.userInfo
+    this.setData({
+      userInfo: userInfo
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   },
   imageLoad: function (e) {
     var windowWidth = 320;
@@ -21,4 +85,14 @@ Page({
       imgheight: windowWidth / 2,
     })
   },
+  Load: function () {
+    wx.navigateTo({
+      url: '../account/index',
+    })
+  },
+  play:function(){
+    wx.navigateTo({
+      url: '../play/index',
+    })
+  }
 })
